86889428bfe9eb773851a879b7323794fbb3ca07
Michael Mukolwe <michaelolukaka@gmail.com>
2023-03-25 03:26:27 +0300

    initialize project and setup the resources

---
+++ resources/js/Pages/Users/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Hello world! {{ label }}</h1>
+
+    <a :href="route('second-route')">Go to second route</a>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  label: String,
+})
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title>Rentz: Tenant Management System</title>
+
+    {{-- Custom Links --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.29.0/feather.min.js" integrity="sha512-24XP4a9KVoIinPFUbcnjIjAjtS59PUoxQj3GNVpWc86bCqPuy3YxAcxJrxFCxXe4GHtAumCbO2Ze2bddtuxaRw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    @routes
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
