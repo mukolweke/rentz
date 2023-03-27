d5c19c4aa688d0ea93c86e11079325371f036e03
Michael Mukolwe <michaelolukaka@gmail.com>
2023-03-27 12:38:15 +0300

    update the path so that the Factory builder class know how to resolve your models.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // src: https://twitter.com/wylesone/status/1303610973736128512?s=21
+        Factory::guessFactoryNamesUsing(function (string $modelName) {
+            // We can also customize where our factories live too if we want:
+            $namespace = 'Database\\Factories\\';
+
+            // Here we are getting the model name from the class namespace
+            $modelName = Str::afterLast($modelName, '\\');
+
+            // Finally we'll build up the full class path where
+            // Laravel will find our model factory
+            return $namespace . $modelName . 'Factory';
+        });
     }
 
     /**
